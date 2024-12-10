cfd50cb235887aa121e03e339766de4d10719b3f
PeterCsorgo <peter.csorgo@icloud.com>
2024-12-10 20:16:30 +0100

    init

---
+++ intro/src/app/services/users.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Usermodel } from '../interfaces/users.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor() { }
+
+  allChars: Array<Usermodel> =[
+    
+  ]
+}
