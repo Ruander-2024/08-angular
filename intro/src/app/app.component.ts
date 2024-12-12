d35592161f3482fa6caa29c0f2f7b34fccc2fdeb
Smiley1144 <kovacsm519@gmail.com>
2024-12-12 20:15:59 +0100

    1212

--- intro/src/app/app.component.ts
+++ intro/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { LandingComponent } from "./modules/landing/landing.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingComponent],
+  imports: [LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
