f823048e4cb5478d6263d76ac5b601fa97e27b34
Pavity Patrik <pavicpatrik@gmail.com>
2024-12-12 22:44:32 +0100

    12.12

--- intro/src/app/app.component.ts
+++ intro/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingComponent } from "./modules/landing/landing.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
