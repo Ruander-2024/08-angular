aa25f321b1bfd39994271fbb354f23a9cd5f4add
Peter-Kohlrusz <peter.kohlrusz@gmail.com>
2024-12-12 20:16:14 +0100

    12.12 Angular

--- intro/src/app/modules/landing/landing.component.ts
+++ intro/src/app/modules/landing/landing.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usermodel } from '../../interfaces/users.model';
+import { UsersService } from '../../services/users.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+  userList: Array<Usermodel> = [];
+  savedUsers: Array<Usermodel> = [];
+
+
+  constructor(private usersService: UsersService){}
+
+  ngOnInit(): void {
+    this.userList = this.usersService.allChars;
+    this.savedUsers = this.usersService.savedUsers;
+  }
+
+  getEven(){
+    this.userList = this.usersService.getEven()
+  }
+
+  getOdds(){
+    this.userList = this.usersService.getOdds()
+  }
+
+  resetTable() {
+    window.location.reload();
+    alert('Your table is refreshed!');
+  }
 
 }
+  
+
