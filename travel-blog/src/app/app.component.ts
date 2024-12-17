709d69cad3eb98a3ee0db6e0c3ac0efa4fc9eb64
Peter-Kohlrusz <peter.kohlrusz@gmail.com>
2024-12-17 20:21:18 +0100

    12.17 travel_2

--- travel-blog/src/app/app.component.ts
+++ travel-blog/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./card/card.component";
+import { MenuComponent } from "./menu/menu.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, CardComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
