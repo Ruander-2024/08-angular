0b943f2ed9a4f0bc968670915aa349c6e65a4695
PeterCsorgo <peter.csorgo@icloud.com>
2024-12-19 19:24:21 +0100

    init

--- travel-blog/src/app/app.component.ts
+++ travel-blog/src/app/app.component.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { MenuComponent } from "./menu/menu.component";
 import { CardComponent } from "./card/card.component";
 import { CommonModule } from '@angular/common';
 
+interface User{
+  name: string;
+  age: number;
+  isActive: boolean;
+}
+
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [MenuComponent, CardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'travel-blog';
+
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  users: User[] | undefined;
+  textColor: string = 'red';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+    this.users = [{
+      name: 'John Doe',
+      age: 19,
+      isActive: true
+    },{
+      name: 'Bill Gates',
+      age: 70,
+      isActive: false
+    },{
+      name: 'Elon Musk',
+      age: 43,
+      isActive: true
+    },{
+      name: 'Teszt User',
+      age: 27,
+      isActive: false
+    }]
+  }, 2500)
+}
+
 }
