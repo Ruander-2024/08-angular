7b4d4e65f9f6279b1aa217a09048307018d903ba
PeterCsorgo <peter.csorgo@icloud.com>
2025-01-14 17:06:00 +0100

    01.14.

--- services-demo/src/app/components/left-counter/left-counter.component.ts
+++ services-demo/src/app/components/left-counter/left-counter.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CounterService } from '../../services/counter.service';
 
 @Component({
   selector: 'app-left-counter',
+  standaloe: true,
   imports: [],
   templateUrl: './left-counter.component.html',
   styleUrl: './left-counter.component.css'
 })
 export class LeftCounterComponent {
+  counter: number = 0;
+
+  constructor(private counterService: CounterService){
+    this.counter = this.counterService.counter
+  }
+
+  addOneToCounter(): void{
+    this.counterService.addCounter();
+    this.counter = this.counterService.counter;
+  }
+
 
 }
--- services-demo/src/app/services/counter.service.ts
+++ services-demo/src/app/services/counter.service.ts
@@ -14,7 +14,9 @@ export class CounterService {
     
   }
 
-  
+  get counter(): number{
+    return this._counter;
+  }
 
 
 
